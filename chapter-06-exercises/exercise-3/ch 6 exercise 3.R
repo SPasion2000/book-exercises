643f8c7717694b34422d391427be259b0f406541
SPasion2000 <59712101+SPasion2000@users.noreply.github.com>
2020-01-17 21:33:42 -0800

    Added Exercises wk2

--- chapter-06-exercises/exercise-3/ch 6 exercise 3.R
+++ chapter-06-exercises/exercise-3/ch 6 exercise 3.R
@@ -10,10 +10,13 @@ ten<-7 + add_three
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-inches_in_meter<-inches * 0.0254
-
-incinches<-12
+inches<-1
+feet<-1
+inches_in_meters<- inches * 0.0254
+feet_in_meters<- feet * .3048
+imperial_to_metric<- inches_in_meters + feet_in_meters
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
-height_in_meters<-  * 0.0254
+height_in_meters<- inches_in_meters*6 + feet_in_meters*5
+height_in_meters
